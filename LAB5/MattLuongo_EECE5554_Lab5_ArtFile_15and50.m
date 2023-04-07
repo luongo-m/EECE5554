b8ba7b3ab6bfd82329e3a8c3f8b644ff0f434ff2
mattluongo <luongo.m@northeastern.edu>
2023-04-06 22:37:44 -0400

    adding m files and report for lab 5

---
+++ LAB5/MattLuongo_EECE5554_Lab5_ArtFile_15and50.m
@@ -0,0 +1,154 @@
+clc
+clear
+close all
+
+% Load images.
+Dir = fullfile('EECE 5554', 'LAB5', '50', {'50_1.jpg'; '50_2.jpg'; ...
+    '50_3.jpg'; '50_4.jpg'; '50_5.jpg'; '50_6.jpg'; '50_7.jpg'});
+% Dir = fullfile('EECE 5554', 'LAB5', '15', {'15_1.jpg'; '15_2.jpg'; ...
+%     '15_3.jpg'; '15_4.jpg'; '15_5.jpg'});
+Scene = imageDatastore(Dir);
+
+% Display images to be stitched.
+montage(Scene.Files)
+
+% Read the first image from the image set.
+I = readimage(Scene,1);
+I = imrotate(I, -90);
+I = imresize(I, [480 480]);
+
+% Initialize features for I(1)
+grayImage = im2gray(I);
+[y,x,m] = harris(grayImage,1000,'tile',[4 4],'disp');
+
+% for 15% overlap
+% [y,x,m] = harris(grayImage,2400,'tile',[8 8],'disp');
+
+locations = [x,y];
+points1 = cornerPoints(locations,"Metric",m);
+[features, points] = extractFeatures(grayImage,points1);
+
+% Initialize all the transformations to the identity matrix. Note that the
+% projective transformation is used here because the building images are fairly
+% close to the camera. For scenes captured from a further distance, you can use
+% affine transformations.
+numImages = numel(Scene.Files);
+tforms(numImages) = projtform2d;
+
+% Initialize variable to hold image sizes.
+imageSize = zeros(numImages,2);
+
+% Iterate over remaining image pairs
+for n = 2:numImages
+    % Store points and features for I(n-1).
+    pointsPrevious = points;
+    featuresPrevious = features;
+
+    % Read I(n).
+    I = readimage(Scene, n);
+    I = imrotate(I, -90);
+    I = imresize(I, [480 480]);
+
+    % Convert image to grayscale.
+    grayImage = im2gray(I);    
+    
+    % Save image size.
+    imageSize(n,:) = size(grayImage);
+
+    % Detect and extract SURF features for I(n).
+    [y,x,m] = harris(grayImage,1000,'tile',[4 4],'disp');
+
+    % for 15% overlap
+    % [y,x,m] = harris(grayImage,2400,'tile',[8 8],'disp');
+
+    locations = [x,y];
+    points1 = cornerPoints(locations,"Metric",m);
+    [features, points] = extractFeatures(grayImage,points1);
+
+    % Find correspondences between I(n) and I(n-1).
+    indexPairs = matchFeatures(features, featuresPrevious, 'Unique', true);
+       
+    matchedPoints = points(indexPairs(:,1), :);
+    matchedPointsPrev = pointsPrevious(indexPairs(:,2), :);        
+
+    % Estimate the transformation between I(n) and I(n-1).
+    tforms(n) = estgeotform2d(matchedPoints, matchedPointsPrev,...
+        'affine', 'Confidence', 99.9, 'MaxNumTrials', 2500);
+    
+    % Compute T(1) * T(2) * ... * T(n-1) * T(n).
+    tforms(n).A = tforms(n-1).A * tforms(n).A; 
+end
+
+% Compute the output limits for each transformation.
+for i = 1:numel(tforms)           
+    [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(i,2)], [1 imageSize(i,1)]);    
+end
+
+avgXLim = mean(xlim, 2);
+[~,idx] = sort(avgXLim);
+centerIdx = floor((numel(tforms)+1)/2);
+centerImageIdx = idx(centerIdx);
+
+Tinv = invert(tforms(centerImageIdx));
+for i = 1:numel(tforms)    
+    tforms(i).A = Tinv.A * tforms(i).A;
+end
+
+for i = 1:numel(tforms)           
+    [xlim(i,:), ylim(i,:)] = outputLimits(tforms(i), [1 imageSize(i,2)], [1 imageSize(i,1)]);
+end
+
+maxImageSize = max(imageSize);
+
+% Find the minimum and maximum output limits. 
+xMin = min([1; xlim(:)]);
+xMax = max([maxImageSize(2); xlim(:)]);
+
+yMin = min([1; ylim(:)]);
+yMax = max([maxImageSize(1); ylim(:)]);
+
+% Width and height of panorama.
+width  = round(xMax - xMin);
+height = round(yMax - yMin);
+
+% Initialize the "empty" panorama.
+panorama = zeros([height width 3], 'like', I);
+
+blender = vision.AlphaBlender('Operation', 'Binary mask', ...
+    'MaskSource', 'Input port');  
+
+% Create a 2-D spatial reference object defining the size of the panorama.
+xLimits = [xMin xMax];
+yLimits = [yMin yMax];
+panoramaView = imref2d([height width], xLimits, yLimits);
+
+% Create the panorama.
+for i = 1:numImages
+    
+    I = imrotate(readimage(Scene, i), -90);   
+    I = imresize(I, [480 480]);
+
+    % Transform I into the panorama.
+    warpedImage = imwarp(I, tforms(i), 'OutputView', panoramaView);
+                  
+    % Generate a binary mask.    
+    mask = imwarp(true(size(I,1),size(I,2)), tforms(i), 'OutputView', panoramaView);
+    
+    % Overlay the warpedImage onto the panorama.
+    panorama = step(blender, panorama, warpedImage, mask);
+end
+
+figure
+imshow(panorama)
+
+
+
+
+
+
+
+
+
+
+
+
